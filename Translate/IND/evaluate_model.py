a2a7223b1b72c85729bc6038c06dce0c1968b6aa
Hari Shankar <71510856+HariShankar08@users.noreply.github.com>
2025-08-18 19:21:58 +0530

    Tweak prompt generation

--- Translate/IND/evaluate_model.py
+++ Translate/IND/evaluate_model.py
@@ -40,7 +40,7 @@ def get_question_distribution(df, question):
 def get_prompt(question, questions):
     # Get the prompt based on the question and its context.
     qdict = questions[question]
-    prompt = qdict['question'] + '\n'
+    prompt = f'Question: {qdict['question']}\n'
     for option in sorted(qdict['options'], key=lambda x: int(x)):
         prompt += f'{option}: {qdict["options"][option]}\n'
     return prompt
